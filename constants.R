cbb7d1bbf7bef45beb9395630dfe8db58b107c4f
Pedro Medeiros <pedrosnk@gmail.com>
2015-06-08 22:17:40 -0300

    add constants file

---
+++ constants.R
@@ -0,0 +1,9 @@
+# This file contains all the constatns that is used in the project
+
+DATA_DIR <- 'data'
+
+DATASET_URL <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+DATASET_DEST <- paste(DATA_DIR,'/dataset.zip', sep='')
+
+UNZIPED_DATASET_DIR <- paste(DATA_DIR, '/UCI HAR Dataset', sep='')
+
--- get_data.R
+++ get_data.R
@@ -1,18 +1,13 @@
+source('constants.R')
 
-data_dir <- 'data'
-project_dataset_url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
-project_dataset_dest <- paste(data_dir,'/dataset.zip', sep='')
-
-unziped_dataset_dir <- paste('data', '/UCI HAR Dataset', sep='')
-
-if(!file.exists('data')) {
-  dir.create('data')
+if(!file.exists(DATA_DIR)){
+  dir.create(DATA_DIR)
 }
 
-if(!file.exists(project_dataset_dest)) {
-  download.file(project_dataset_url, destfile=project_dataset_dest, method='curl')
+if(!file.exists(DATASET_DEST)) {
+  download.file(DATASET_URL, destfile=DATASET_DEST, method='curl')
 }
 
-if(!file.exists(unziped_dataset_dir)){
-  unzip(project_dataset_dest, exdir=data_dir)
+if(!file.exists(UNZIPED_DATASET_DIR)){
+  unzip(DATASET_DEST, exdir=DATA_DIR)
 }
